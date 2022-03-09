5a8ee2736f1e1392e45401b480bda3dff6019e01
EmJCanton <EmmaJane-Canton@live.nmit.ac.nz>
2022-03-09 13:56:54 +1300

    created a multi-line comment in app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 console.log("Hello World")
+// TODO:
+
+// NOTE: 
+/*
+Ola amigooooooos!
+I can write whatever here!
+Because I have used the group comment tags! (slash asterisk to open, asterisk slash to close)
+
+If I want to run this piece of javascript, I go to the command line and type in "node app.js"
+Try it now!
+*/
