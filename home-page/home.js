7e935a23ac06099571a8f986ac96ff405fdcded0
EmJCanton <EmmaJane-Canton@live.nmit.ac.nz>
2022-03-30 11:24:17 +1300

    added script to home.js

---
+++ home-page/home.js
@@ -0,0 +1,10 @@
+var myName = "Lawrence";
+var myCourse = "JavaScript";
+console.log(myName)
+alert("Welcome");
+var myAge = 40;
+console.log(myAge);
+
+/*
+You can write a bunch of stuff here and nothing will be rendered out. Scret for Web Developers
+*/
