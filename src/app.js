7b747524ecf3f9bbd9a958b7e0b9b96174729368
EmJCanton <EmmaJane-Canton@live.nmit.ac.nz>
2022-03-30 11:08:09 +1300

    put javascript from html file into app.js and changed script tags

--- src/app.js
+++ src/app.js
@@ -11,3 +11,27 @@ Because I have used the group comment tags! (slash asterisk to open, asterisk sl
 If I want to run this piece of javascript, I go to the command line and type in "node app.js"
 Try it now!
 */
+
+// Print "Hello World"
+console.log("Hello World")
+// Declare Variable "a" with value of "Hello World" (value in quotations is a string)
+var a = "Hello World";
+// Print Variable "a"
+console.log(a);
+
+// Declare variable "myName"
+var myName = "Lawrence";
+// Declare variable "myCourse"
+var myCourse = "JavaScript";
+// Print "myName"
+console.log(myName)
+// give a browser pop-up alert message
+// alert("Welcome");
+// Declare variable "myAge"
+var myAge = 40;
+// Print variable "myAge"
+console.log(myAge);
+
+document.querySelector("button").addEventListener("click", function (){
+    document.querySelector("div").textContent = "Hello, " + document.querySelector("input").value;
+})
