fcd856494c6a49a8a8382d333fc94d054d370e21
EmJCanton <EmmaJane-Canton@live.nmit.ac.nz>
2022-03-30 11:33:52 +1300

    new stuff with comments in home.js

--- home-page/home.js
+++ home-page/home.js
@@ -1,10 +1,20 @@
-var myName = "Lawrence";
+var notMyName = "Lawrence";
 var myCourse = "JavaScript";
-console.log(myName)
-alert("Welcome");
+console.log(notMyName)
+// alert("Welcome");
 var myAge = 40;
 console.log(myAge);
 
 /*
-You can write a bunch of stuff here and nothing will be rendered out. Scret for Web Developers
+You can write a bunch of stuff here and nothing will be rendered out. Secret for Web Developers
 */
+
+// Declare a welcome variable
+var welcome = "Welcome to My Space!"
+// Print the message
+console.log(welcome)
+
+// Declare variable my name, with a value of my friend's name
+var myName = "Jessica"
+//print the variable
+console.log(myName)
