ee6ecdf2a5186758312afe1221e325b7c098ebf0
EmJCanton <EmmaJane-Canton@live.nmit.ac.nz>
2022-04-06 10:24:40 +1200

    new funtion

--- wk7paircode/wk7app.js
+++ wk7paircode/wk7app.js
@@ -1,10 +1,19 @@
-// Create a function that takes an array containing only numbers and return the first element.
-// Example: gatArrElement([1, 2, 3, 4]) -> 1
+// // Create a function that takes an array containing only numbers and return the first element.
+// // Example: gatArrElement([1, 2, 3, 4]) -> 1
 
-function getArrElement(arr) {
-return arr[0]
+// function getArrElement(arr) {
+// return arr[0]
+// }
+
+// console.log(getArrElement([1,2,3,4]))
+
+// const getOneLineArrElement = arr => arr [0]
+
+// Give two arguments, return an array which contains the sum of these two arguments
+
+function createArray(a, b) {
+    var arr = [a, b]
+    return arr
 }
 
-console.log(getArrElement([1,2,3,4]))
-
-const getOneLineArrElement = arr => arr [0]
+console.log (createArray(1, 2))
