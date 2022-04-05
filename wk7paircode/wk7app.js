8e09764d363f4eb04affd71a16b81a8c5b253b55
LaVieBlossom <abigail-nisbett@live.nmit.ac.nz>
2022-04-06 09:30:14 +1200

    function

--- wk7paircode/wk7app.js
+++ wk7paircode/wk7app.js
@@ -1,2 +1,8 @@
 // Create a function that takes an array containing only numbers and return the first element.
 // Example: gatArrElement([1, 2, 3, 4]) -> 1
+
+function getArrElement(arr) {
+return arr[0]
+}
+
+console.log(getArrElement([1,2,3,4]))
