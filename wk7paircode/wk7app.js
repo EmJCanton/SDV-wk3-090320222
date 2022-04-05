6b7c2f4f0f8c0887e7eb951db3934773c50b9cd4
EmJCanton <EmmaJane-Canton@live.nmit.ac.nz>
2022-04-06 11:15:39 +1200

    new return only last item in an array

--- wk7paircode/wk7app.js
+++ wk7paircode/wk7app.js
@@ -18,14 +18,22 @@
 
 // console.log (createArray(1, 2))
 
-// write a function to reverse an array
-// Example: reverseArray ([1, 2, 3, 4]) -> [4, 3, 2, 1]
+// // write a function to reverse an array
+// // Example: reverseArray ([1, 2, 3, 4]) -> [4, 3, 2, 1]
 
-function reverseArray(arr) {     // Putting arr into the funtion brackets tells JS you will input an array?
-    return arr.reverse()     // JS .reverse() is an inbuilt function
+// function reverseArray(arr) {     // Putting arr into the funtion brackets tells JS you will input an array?
+//     return arr.reverse()     // JS .reverse() is an inbuilt function
+// }
+
+// console.log(reverseArray([1,2,3,4,5,6,7,8,9]))  // Put the array into square brackets to tell JS it is an array
+
+// const oneReverseArray = arr => arr.reverse(); // same funtion in one line
+// console.log(oneReverseArray([2,4,6,8,0]))
+
+// Create a function that accepts an array and returns the last element
+// Example lastElement(["Ali", 12, "blue", "green"]) -> Green
+
+function lastElement(arr) {
+    return arr.slice(-1)
 }
-
-console.log(reverseArray([1,2,3,4,5,6,7,8,9]))  // Put the array into square brackets to tell JS it is an array
-
-const oneReverseArray = arr => arr.reverse(); // same funtion in one line
-console.log(oneReverseArray([2,4,6,8,0]))
+console.log(lastElement(["ali", 12, "blue", "green"]))
