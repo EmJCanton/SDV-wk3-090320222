4599afd12c6dedc39daf5dbe54d637e66e55ed24
LaVieBlossom <abigail-nisbett@live.nmit.ac.nz>
2022-04-06 14:34:11 +1200

    findMaxnumbers

--- wk7paircode/wk7app.js
+++ wk7paircode/wk7app.js
@@ -101,3 +101,19 @@ Example: objectToArray({a: "Ali", b:12}) -> [["a", "Ali"],["b",12]]
 // }
 // // ES6
 // const objToArr = obj => Object.entries(obj)
+
+//create a function that takes an array of arrays and returns a single new array with largest numbers of each.
+//Example: findMaxNumbers([[1,2,3],[4,5,6],[7,8,9]]) -> [3,6,9]
+function findMaxnumbers(arr) {
+    Let newArr = []
+    for(Let i = 0; i < arr.length; i++){
+        Let maxNumber = arr[i][0]
+        for(Let j = 0; j < arr[i].length; j++){
+            if(arr[i][j] > maxNumber) {
+                maxNumber = arr[i][j]
+            }
+        }
+        newArr.push(maxNuumber)
+    }
+    return newArr
+}
